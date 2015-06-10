ffacae1ec81fddc4a9429054b67e1410f0deedca
Greg Davis <antelope.buzkowksi@gmail.com>
2015-06-10 00:16:38 -0600

    progress on module pattern

---
+++ bin/add.py
@@ -0,0 +1,3 @@
+def add(q):
+    qs = q.split(" ")
+    print "Your answer is: %r" % (float(qs[1]) + float(qs[3]))
---
+++ vger.py
@@ -0,0 +1,64 @@
+import os
+
+class Vger :
+    # define the new array
+    bindings = {}
+    names = []
+    # initialize Vger
+    def _init(self) :
+        print "Intializing Vger"
+        
+        # bring in dependencies, and assign their names
+        # to an array on Vger of the defined functions
+        print "Loading the following modules"
+        files = os.listdir("./bin")
+        for file in files:
+            if ("__init__" not in file) and (".pyc" not in file) :
+                file = file.replace(".py", "")
+                print file
+                self.bindings[file] = (__import__('bin.'+file, fromlist=["bin"]))
+                self.names.append(file)        
+        
+        print "I am Vger. What can I do for you?"
+
+    # ask function to process questions
+    def ask(self, q) :
+        # ask a new question
+        def askNew() :
+            qnew = raw_input(prompt)
+            self.ask(qnew)
+
+        # lowercase the question for better comparisons
+        q = q.lower()
+        
+        # find out if the command is available
+        isCommand = False
+        
+        for name in self.names :
+            if (name in q) :
+                isCommand = True
+                command = name
+        
+        if (q == "quit") or (q == "q") :
+            print "quitting"
+        elif ("how are you" in q) or ("how ya doing" in q) or ("how's it hanging" in q) :
+            # a simple response to a how are you
+            print "I am fine, thank you."
+            self.askNew()
+        elif (isCommand == True) :
+            getattr(self.bindings[command], command)(q)
+            askNew()
+        else :
+            print "I don't understand your command, %s" % q
+            askNew()
+
+
+vger = Vger()
+prompt = ">>"
+
+# run the init on Vger
+vger._init()
+# ask the first question to start the looping
+q = raw_input(prompt)
+vger.ask(q)
+
