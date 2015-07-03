b1dc1d00d0a374cbef3c3903e2af5eb40bfa8223
Lacey Dog <laceydog@laceydog.local>
2015-07-03 16:11:39 -0600

    local changes from lab computer

---
+++ bin/calculate.py
@@ -0,0 +1,5 @@
+# Calculator for Vger, should be able to take an argument of varying
+# complexity, such as addition, subtraction, roots, powers, etc.
+def calculate(q) :
+	# take the question and evaluate what is being asked
+	print "I do not know how to calculate yet. Sorry"	
---
+++ bin/max.py
@@ -0,0 +1,4 @@
+def max(q):
+	print "you want to love it buddy"
+	print "you want to love it"
+	print "oh thats a good boy"
